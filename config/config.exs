581bc1c94650d03d470f181cd147d758f98b331d
Grant Powell <gjamespowell@gmail.com>
2019-10-07 19:50:28 -0400

    Initial

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :weather, WeatherWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "D4J/ZrKH/384ND38l0Y+j+5PHP664OYZbjoJEqIbFuuCljx2JlMvW6dQ7nxB1kwc",
+  render_errors: [view: WeatherWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Weather.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
