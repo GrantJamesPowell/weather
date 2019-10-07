f5990c606b79fafb5329af6f3201934d6dbbe026
Grant Powell <gjamespowell@gmail.com>
2019-10-07 19:56:48 -0400

    Spring clean things left over from

--- lib/weather_web.ex
+++ lib/weather_web.ex
@@ -1,22 +1,4 @@
 defmodule WeatherWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use WeatherWeb, :controller
-      use WeatherWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
-
   def controller do
     quote do
       use Phoenix.Controller, namespace: WeatherWeb
@@ -33,10 +15,8 @@ defmodule WeatherWeb do
         root: "lib/weather_web/templates",
         namespace: WeatherWeb
 
-      # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
-      # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
       import WeatherWeb.ErrorHelpers
@@ -60,9 +40,6 @@ defmodule WeatherWeb do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
