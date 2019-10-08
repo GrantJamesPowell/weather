88cce59cb865230db3bd36367627a23f11ef3e97
Grant Powell <gjamespowell@gmail.com>
2019-10-07 23:03:11 -0400

    mix format

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,6 @@ config :weather, WeatherWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
-
 # Do not print debug messages in production
 config :logger, level: :info
 
