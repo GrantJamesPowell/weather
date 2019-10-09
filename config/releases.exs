a212f09f78cdaeed629fe4a6b8baa871592f3247
Grant Powell <gjamespowell@gmail.com>
2019-10-08 21:12:10 -0400

    Read envs to configure a release

--- config/releases.exs
+++ config/releases.exs
@@ -1,3 +1,8 @@
 import Config
 
-config :weather, :secret_key_base, System.fetch_env!("SECRET_KEY_BASE")
+config :weather, WeatherWeb.Endpoint,
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+  http: [port: System.get_env("PORT") || 4000],
+  server: true
+
+config :weather, dark_sky_api_key: System.fetch_env!("DARK_SKY_API_KEY")
